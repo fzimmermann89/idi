70e1875296a8463585513cd4c651333168ed7c71
Felix Zimmermann <fzimmermann89@gmail.com>
2019-05-16 03:46:56 -0700

    recon: fix simple

--- idi/reconstruction/simple.py
+++ idi/reconstruction/simple.py
@@ -1,8 +1,8 @@
-import numpy as np
+import numpy as _np
+from idi.util import abs2
 
-
-def corr(i_nput):
-    ret = _np.zeros([2 * s for s in i_nput.shape])
-    ret[: i_nput.shape[0], : i_nput.shape[1]] = i_nput
-    ret = _np.fft.fftshift(_np.abs(_np.fft.irfftn(_np.square(_np.abs(_np.fft.rfftn(ret))))))
+def corr(input):
+    ret = _np.zeros([2 * s for s in input.shape])
+    ret[:input.shape[0], :input.shape[1]] = input
+    ret = _np.fft.fftshift(_np.fft.irfftn(abs2(_np.fft.rfftn(ret))))
     return ret
