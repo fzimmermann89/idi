5d26290d06b3cc5df0ccbe85d859552bf54e4959
Felix Zimmermann <fzimmermann89@gmail.com>
2019-07-19 02:49:15 +0900

    add fftfilter

--- idi/util.py
+++ idi/util.py
@@ -254,7 +254,7 @@ class accumulator:
 
 def filter_std(image, size, sigma=1):
     '''
-    filters an nd image based on deviation by local stdev over local mean.
+    explicitly filters an nd image based on deviation by local stdev over local mean.
     return mask of points that are sigma local stdev over(under) the local mean if sigma is positiv(negativ)
     '''
     import numpy as np
@@ -281,4 +281,22 @@ def filter_std(image, size, sigma=1):
     ptr = ctypes.cast(ctypes.pointer(csigma), ctypes.c_void_p)
     res = image>ndi.generic_filter(image, LowLevelCallable(_std.ctypes, ptr), size)
     if sigma<0: res = ~res
+    return res
+
+
+
+import scipy.signal as _ss
+def fftfilter_mean(image,size, norm=False):
+    strel=_np.ones(image.ndim*[size])/size**image.ndim
+    res=_ss.fftconvolve(image,strel,mode='same')
+    if norm: res/=_ss.fftconvolve(_np.ones_like(image),strel,mode='same')
+    return res
+
+def fftfilter_std(image,size,sigma):
+    norm=fftfilter_mean(_np.ones_like(image),size)
+    mean=fftfilter_mean(image,size)/norm
+    sqmean=fftfilter_mean(image*image,size)/norm
+    std=_np.nan_to_num(_np.sqrt(sqmean-mean*mean))
+    res=_np.abs(image-mean)>sigma*std
+    res[_np.abs(sigma*std)<1e-15]=0
     return res
