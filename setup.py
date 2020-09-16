a2ce5af7925cf8243ad7fc90c3cea1cfa83b7365
Felix Zimmermann <fzimmermann89@gmail.com>
2020-09-16 10:01:37 -0700

    setup: dependencies - replace jinja with jinja2, add matplotlibwq

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ def setup_package():
         description='idi simulation and reconstruction',
         platforms=['Linux', 'Mac OS-X'],
         python_requires='>=3.6',
-        install_requires=['numpy', 'cython', 'numba', 'numexpr', 'scipy', 'jinja', 'mkl-devel'],
+        install_requires=['numpy', 'cython', 'numba', 'numexpr', 'scipy', 'jinja2', 'mkl-devel', 'matplotlib'],
         scripts=['idi_sim.py', 'idi_simrecon.py', 'idi_simrecon_normalize.py'],
         configuration=configuration,
     )
