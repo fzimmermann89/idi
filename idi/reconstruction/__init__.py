22c716228eba1d522b289094ee07b9c310a7a520
Felix Zimmermann <fzimmermann89@gmail.com>
2020-09-30 04:27:15 -0700

    allow auto switching between cpu and cuda code

--- idi/reconstruction/__init__.py
+++ idi/reconstruction/__init__.py
@@ -1,9 +1,12 @@
-__all__ = [ 'hitcor', 'hitcorrad', 'qcor', 'qcorrad', 'cucor', 'cucorrad', 'simple','cusimple', 'ft']
+__all__ = [ 'hitcor', 'hitcorrad', 'cpucor', 'cpucorrad', 'cucor', 'cucorrad', 'cpusimple','cusimple', 'ft']
 
 import mkl as _mkl
+import numba.cuda as _nbcuda
+import warnings as _w
+
 _vml_threads=_mkl.domain_get_max_threads('vml')
 
-from . import hitcor, hitcorrad, qcor, qcorrad, cucor, cucorrad, simple, common, cusimple
+from . import hitcor, hitcorrad, cpucor, cpucorrad, cpusimple, common
 
 try:
     from . import autocorrelate3
@@ -20,12 +23,27 @@ if _local:
         _np_inc = [_np_get_include()]
         _pyx.install(setup_args={'include_dirs': _mkl_inc + _np_inc }, language_level=2)
     except ImportError:
-        import warnings as _w
-
-        _w.warn("no cython!")
+        _w.warn("ft autocorrelate")
     else:
         from . import ft
 else:
     from . import ft
 
+
+_cuda = _nbcuda.is_available()
+try:
+    from . import cucor, cucorrad, cusimple
+    simple = cusimple
+    qcor = cucor
+    qcorrad = cucorrad
+    
+except Exception as e:
+    print(e)
+    _cuda = False
+if not _cuda:
+    _w.warn("no cuda")
+    simpe = cpusimple
+    qcor = cpucor 
+    qcorrad = cpucorrad
+    
 _mkl.domain_set_num_threads(_vml_threads,'vml') #numexpr messes with vml thread number
--- idi/simulation/__init__.py
+++ idi/simulation/__init__.py
@@ -3,9 +3,11 @@ from . import cpu, simobj, common
 
 try:
     from . import cuda
+    auto = cuda
 except ImportError as _e:
     if "cuda" in _e.args[0] or "libcu" in _e.args[0]:
         import warnings as _w
-        _w.warn('cuda error. cuda simulation not imported. is cuda in path?')
+        _w.warn('cuda error. cuda simulation not imported. is cuda available and nvcc path?')
+        auto = cpu
     else:
         raise _e
