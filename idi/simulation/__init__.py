4bb4f9d8368aad1b571ecc132fe1c83623b055e3
Felix Zimmermann <fzimmermann89@gmail.com>
2021-01-13 09:59:53 -0800

    change init

--- idi/simulation/__init__.py
+++ idi/simulation/__init__.py
@@ -1,14 +1,22 @@
-__all__ = ['cpu', 'simobj', 'cuda', 'common']
+__all__ = ['cpu', 'simobj', 'cuda', 'common', 'time', 'cutime']
 from . import cpu, simobj, common, time
 
 try:
     from . import cuda
-    from . import cutime
     auto = cuda
 except ImportError as _e:
     if "cuda" in _e.args[0] or "libcu" in _e.args[0]:
         import warnings as _w
-        _w.warn('cuda error. cuda simulation not imported. is cuda available and nvcc path?')
+        _w.warn('cuda error. cuda simulation not imported. is cuda available and paths set?')
         auto = cpu
     else:
         raise _e
+try:
+    from . import cutime
+    autotime = cutime
+except (AttributeError,ImportError) as _e:
+      if "cuda" in _e.args[0] or "libcu" in _e.args[0] or "split" in _e.args[0]:
+        import warnings as _w
+        _w.warn('cuda error. cuda time dependent simulation not imported. is cuda available and paths set?')
+        autotime = time
+  
