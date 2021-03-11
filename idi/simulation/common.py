14fd1389843b701a8e15a7771f388a67cc88483d
Felix Zimmermann <fzimmermann89@gmail.com>
2021-03-11 14:37:41 +0100

    charge sharing

--- idi/simulation/common.py
+++ idi/simulation/common.py
@@ -1,5 +1,6 @@
 import numpy as _np
 import numba as _numba
+from math import erf, sqrt
 
 
 def randomphotons(probs, Nphotons, rng=None, dtype=float):
@@ -10,33 +11,37 @@ def randomphotons(probs, Nphotons, rng=None, dtype=float):
 
 def chargesharing(img, s):
     """
-    simulates charge charing between neighboring pixels:
-    for each count in the discrete image img, a uniformly random position within the pixel is chosen
-    and a gaussian distribution with sigma s applied to the neighboring pixels.
+    simulates charge sharing between neighboring pixels:
+    for each count in the discrete image img, a uniformly random position within the pixel is chosen 
+    and a Gaussian distribution with sigma s applied to the neighboring pixels.
+    charge that leaves the detector is lost.
     """
 
     @_numba.njit(fastmath=True, cache=True)
     def _put(ret, hits, s, w):
         mu = _np.random.rand(len(hits), 2) - 0.5
-        v = _np.zeros((2 * w + 1) * (2 * w + 1))
+        k = _np.zeros((2 * w + 1, 2 * w + 1))
+        c = 1 / (2 * sqrt(2) * s)
         for i in range(len(hits)):
-            hitx, hity = hits[i]
+            k[:] = 1 / 4
             mux, muy = mu[i]
-            j = 0
-            for ox in range(-w, w + 1):
-                for oy in range(-w, w + 1):
-                    v[j] = _np.exp(-((ox - mux) ** 2 + (oy - muy) ** 2) / (2 * s ** 2))
-                    j += 1
-            v = v / _np.sum(v)
-            j = 0
-            for ox in range(-w, w + 1):
-                for oy in range(-w, w + 1):
-                    ret[hitx + ox, hity + oy] += v[j]
-                    j += 1
+            for x in range(-w, w + 1):
+                a = (mux - x) / (sqrt(2) * s)
+                k[x + w, :] *= erf(c + a) + erf(c - a)
+            for y in range(-w, w + 1):
+                b = muy - y / (sqrt(2) * s)
+                k[:, y + w] *= erf(c + b) + erf(c - b)
+            hitx, hity = hits[i]
+            for x in range(2 * w + 1):
+                for y in range(2 * w + 1):
+                    ret[hitx + y - w, hity + x - w] += k[x, y]
+
+    if not s > 0:
+        return img
 
     w = int(_np.ceil(2 * s))
+    hits = _np.repeat(_np.indices(img.shape).reshape(2, -1), img.astype(int).ravel(), axis=1).T + _np.array((w, w))
     ret = _np.zeros((img.shape[0] + 2 * w, img.shape[1] + 2 * w))
-    hits = _np.repeat(_np.indices(img.shape).reshape(2, -1), img.ravel(), axis=1).T
     _put(ret, hits, s, w)
     ret = ret[w:-w, w:-w]
     return ret
