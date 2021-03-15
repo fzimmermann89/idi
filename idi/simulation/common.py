4e24ce90bdf0a9a07079f72aa3295560f9b66664
Felix Zimmermann <fzimmermann89@gmail.com>
2021-03-15 22:25:04 +0100

    charge sharing

--- idi/simulation/common.py
+++ idi/simulation/common.py
@@ -17,31 +17,31 @@ def chargesharing(img, s):
     charge that leaves the detector is lost.
     """
 
-    @_numba.njit(fastmath=True, cache=True)
-    def _put(ret, hits, s, w):
-        mu = _np.random.rand(len(hits), 2) - 0.5
-        k = _np.zeros((2 * w + 1, 2 * w + 1))
-        c = 1 / (2 * sqrt(2) * s)
-        for i in range(len(hits)):
-            k[:] = 1 / 4
-            mux, muy = mu[i]
-            for x in range(-w, w + 1):
-                a = (mux - x) / (sqrt(2) * s)
-                k[x + w, :] *= erf(c + a) + erf(c - a)
-            for y in range(-w, w + 1):
-                b = muy - y / (sqrt(2) * s)
-                k[:, y + w] *= erf(c + b) + erf(c - b)
-            hitx, hity = hits[i]
-            for x in range(2 * w + 1):
-                for y in range(2 * w + 1):
-                    ret[hitx + y - w, hity + x - w] += k[x, y]
-
     if not s > 0:
         return img
 
     w = int(_np.ceil(2 * s))
     hits = _np.repeat(_np.indices(img.shape).reshape(2, -1), img.astype(int).ravel(), axis=1).T + _np.array((w, w))
     ret = _np.zeros((img.shape[0] + 2 * w, img.shape[1] + 2 * w))
-    _put(ret, hits, s, w)
+    __putcharges(ret, hits, s, w)
     ret = ret[w:-w, w:-w]
     return ret
+
+@_numba.njit(fastmath=True, cache=True)
+def __putcharges(ret, hits, s, w):
+    mu = _np.random.rand(len(hits), 2) - 0.5
+    k = _np.zeros((2 * w + 1, 2 * w + 1))
+    c = 1 / (2 * sqrt(2) * s)
+    for i in range(len(hits)):
+        k[:] = 1 / 4
+        mux, muy = mu[i]
+        for x in range(-w, w + 1):
+            a = (mux - x) / (sqrt(2) * s)
+            k[x + w, :] *= erf(c + a) + erf(c - a)
+        for y in range(-w, w + 1):
+            b = muy - y / (sqrt(2) * s)
+            k[:, y + w] *= erf(c + b) + erf(c - b)
+        hitx, hity = hits[i]
+        for x in range(2 * w + 1):
+            for y in range(2 * w + 1):
+                ret[hitx + y - w, hity + x - w] += k[x, y]
