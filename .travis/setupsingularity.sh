7d6cdf3d29dbaf6033c6b22a52d32f752aa86615
Felix Zimmermann <fzimmermann89@gmail.com>
2021-04-17 20:07:13 +0200

    fix travis

--- .travis/setupsingularity.sh
+++ .travis/setupsingularity.sh
@@ -5,9 +5,9 @@ sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
 # Check Python
 
 echo "Python Version:"
-python --version
-pip install --user sregistry[all]
-sregistry version
+#python --version
+#pip install --user sregistry[all]
+#sregistry version
 
 echo "sregistry Version:"
 
@@ -19,7 +19,9 @@ export PATH="${GOPATH}/bin:${PATH}"
 mkdir -p "${GOPATH}/src/github.com/sylabs"
 cd "${GOPATH}/src/github.com/sylabs"
 
-git clone -b release-3.3 https://github.com/sylabs/singularity
+export VERSION=3.7.0 
+wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
+tar -xzf singularity-${VERSION}.tar.gz
 cd singularity
 ./mconfig -v -p /usr/local
 make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
