84cc0afec73942c56b23a5bd8bda3972c0569f2c
Felix Zimmermann <fzimmermann89@gmail.com>
2021-04-21 00:37:54 +0200

    cp setupsingularity.sh

---
+++ .github/setupsingularity.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -ex
+#https://github.com/singularityhub/travis-ci/blob/master/.travis/setup.sh
+sudo sed -i -e 's/^Defaults\tsecure_path.*$//' /etc/sudoers
+
+# Check Python
+
+echo "Python Version:"
+#python --version
+#pip install --user sregistry[all]
+#sregistry version
+
+echo "sregistry Version:"
+
+# Install Singularity
+
+SINGULARITY_BASE="${GOPATH}/src/github.com/sylabs/singularity"
+export PATH="${GOPATH}/bin:${PATH}"
+
+mkdir -p "${GOPATH}/src/github.com/sylabs"
+cd "${GOPATH}/src/github.com/sylabs"
+
+export VERSION=3.7.0 
+wget https://github.com/hpcng/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
+tar -xzf singularity-${VERSION}.tar.gz
+cd singularity
+./mconfig -v -p /usr/local
+make -j `nproc 2>/dev/null || echo 1` -C ./builddir all
+sudo make -C ./builddir install
