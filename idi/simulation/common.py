acf91da44488ca9b1d95f0b10ec87c8e5f07ddeb
Felix Zimmermann <fzimmermann89@gmail.com>
2021-07-03 10:28:25 -0700

    fix sharing

--- idi/simulation/common.py
+++ idi/simulation/common.py
@@ -9,7 +9,7 @@ def randomphotons(probs, Nphotons, rng=None, dtype=float):
     return rng.poisson(probs * (Nphotons / probs.sum(axis=(-2, -1), keepdims=True))).astype(dtype)
 
 
-def chargesharing(img, s):
+def chargesharing(img, s, ns=0):
     """
     simulates charge sharing between neighboring pixels:
     for each count in the discrete image img, a uniformly random position within the pixel is chosen
@@ -23,16 +23,18 @@ def chargesharing(img, s):
     w = int(_np.ceil(2 * s))
     hits = _np.repeat(_np.indices(img.shape).reshape(2, -1), img.astype(int).ravel(), axis=1).T + _np.array((w, w))
     ret = _np.zeros((img.shape[0] + 2 * w, img.shape[1] + 2 * w))
-    __putcharges(ret, hits, s, w)
+    __putcharges(ret, hits, s, w, ns)
     ret = ret[w:-w, w:-w]
     return ret
 
 
 @_numba.njit(fastmath=True, cache=True)
-def __putcharges(ret, hits, s, w):
+def __putcharges(ret, hits, s, w, ns):
     mu = _np.random.rand(len(hits), 2) - 0.5
     k = _np.zeros((2 * w + 1, 2 * w + 1))
     c = 1 / (2 * sqrt(2) * s)
+    if ns > 0:
+        scale = 1 + _np.random.randn(len(hits)) * ns
     for i in range(len(hits)):
         k[:] = 1 / 4
         mux, muy = mu[i]
@@ -40,9 +42,9 @@ def __putcharges(ret, hits, s, w):
             a = (mux - x) / (sqrt(2) * s)
             k[x + w, :] *= erf(c + a) + erf(c - a)
         for y in range(-w, w + 1):
-            b = muy - y / (sqrt(2) * s)
+            b = (muy - y) / (sqrt(2) * s)
             k[:, y + w] *= erf(c + b) + erf(c - b)
         hitx, hity = hits[i]
         for x in range(2 * w + 1):
             for y in range(2 * w + 1):
-                ret[hitx + y - w, hity + x - w] += k[x, y]
+                ret[hitx + y - w, hity + x - w] += scale[i] * k[x, y] if ns > 0 else k[x, y]
