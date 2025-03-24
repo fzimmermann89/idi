15e4a02ec8ad4c6a04b383ab56c38601647fdb42
Felix Zimmermann <fzimmermann89@gmail.com>
2025-03-24 14:52:22 +0100

    update

--- setup.py
+++ setup.py
@@ -7,17 +7,6 @@ import setuptools  # noqa # TODO
 from distutils.command.sdist import sdist
 
 
-def aspath(x):
-    """for python 3.8 on github actions"""
-    try:
-        x = realpath(x)
-        if not exists(x):
-            return None
-        return abspath(x)
-    except:
-        return None
-
-
 def configuration():
     from numpy.distutils.misc_util import Configuration
     from numpy.distutils.system_info import (
@@ -33,7 +22,7 @@ def configuration():
     mkl_info = get_info("mkl")
     basedirs = list(
         OrderedDict.fromkeys(
-            aspath(p)
+            p
             for p in [
                 join(dirname(numpy.__file__), *(4 * [".."])),
                 join(dirname(numpy.__file__), *(3 * [".."])),
@@ -42,7 +31,6 @@ def configuration():
             + [join(*p, *(2 * [".."])) for p in [p.split("site-packages")[:-1] for p in path] if p]
             + [join(*p, "..") for p in [p.split("site-packages")[:-1] for p in path] if p]
             + [join(p, "..") for p in environ["PATH"].split(pathsep)]
-            if aspath(p)
         )
     )
     include_dirs = [
