6d650d42506da6253485b5eecfff408a967c46a2
Felix Zimmermann <fzimmermann89@gmail.com>
2025-03-24 15:00:28 +0100

    add corr class from lv65

--- idi/reconstruction/cpusimple.py
+++ idi/reconstruction/cpusimple.py
@@ -1,14 +1,6 @@
 import numpy as _np
 from ..util import fastlen as _fastlen
 
-# import numexpr as _ne
-# from ..util import abs2
-# def corr(input):
-#     ret = _np.zeros([2 * s for s in input.shape])
-#     ret[:input.shape[0], :input.shape[1]] = input
-#     ret = _np.fft.fftshift(_np.fft.irfftn(abs2(_np.fft.rfftn(ret))))
-#     return ret
-
 
 def corr(input, axes=(-2, -1), norm=False, fftfunctions=(_np.fft.rfftn, _np.fft.irfftn), **kwargs):
     """
@@ -34,3 +26,81 @@ def corr(input, axes=(-2, -1), norm=False, fftfunctions=(_np.fft.rfftn, _np.fft.
         ret /= n
         ret[(...,) + (n < 0.9).nonzero()] = _np.nan
     return ret
+
+
+class correlator2D:
+    def __init__(self, mask=None, norm: bool = True) -> None:
+        """Simple 2D correlator
+        Does the 2D autocorrelation divided by the mask autocorrelation.
+        mask: mask to use for the autocorrelation or None for all-ones mask
+        norm: normalize input by mean over all pixels.
+        """
+        self._fftshape = None
+        if mask is not None:
+            mask = _np.asarray(mask, dtype=_np.float32)
+            self._maskcorr = self._corr(mask)
+            self._mask = mask
+        else:
+            self._mask = None
+            self._maskcorr = None
+        self._mean = None
+        self._n = 0
+        self._nvar = None
+        self._norm = norm
+
+    def _corr(self, input: _np.ndarray) -> _np.ndarray:
+        if self._fftshape is None:
+            self._fftshape = [_fastlen(2 * s) for s in input.shape]
+        ret = _np.fft.rfft2(input, s=self._fftshape)
+        ret = _np.abs(ret) ** 2
+        ret = _np.fft.irfft2(ret)
+        ret = _np.fft.fftshift(ret, axes=(-2, -1))[
+            ...,
+            slice(self._fftshape[-2] // 2 - input.shape[-2], self._fftshape[-2] // 2 + input.shape[-2]),
+            slice(self._fftshape[-1] // 2 - input.shape[-1], self._fftshape[-1] // 2 + input.shape[-1]),
+        ]
+        return ret
+
+    def add(self, input: _np.ndarray, mask=None) -> None:
+        if input.ndim != 2:
+            raise ValueError("input must be 2D.")
+        input = _np.array(input, dtype=_np.float32, copy=True)
+        if self._norm:
+            input /= _np.mean(input)
+        if mask is not None:
+            input *= mask
+            maskcorr = self._corr(mask)
+        elif self._mask is not None:
+            input *= self._mask
+            maskcorr = self._maskcorr
+        elif self._maskcorr is not None:
+            maskcorr = self._maskcorr
+        else:
+            mask = _np.ones_like(input)
+            maskcorr = self._corr(mask)
+            self._maskcorr = maskcorr
+
+        corr = _np.divide(self._corr(input), maskcorr, where=maskcorr > 0.5, out=_np.zeros_like(maskcorr))
+
+        if self._mean is None:
+            self._mean = corr
+            self._n = 1
+            self._nvar = _np.zeros_like(corr)
+        else:
+            self._n += 1
+            delta = corr - self._mean
+            self._mean += delta / self._n
+            delta2 = corr - self._mean
+            self._nvar += delta * delta2
+
+    @property
+    def mean(self) -> _np.ndarray:
+        return self._mean
+
+    @property
+    def n(self) -> int:
+        return self._n
+
+    @property
+    def std(self) -> _np.ndarray:
+        return _np.sqrt(self._nvar / self._n)
--- idi/reconstruction/cusimple.py
+++ idi/reconstruction/cusimple.py
@@ -20,7 +20,12 @@ def corr(input, axes=(-1, -2), norm=False, returngpu=False, **kwargs):
     ret = _cp.abs(ret) ** 2
     ret = _cp.fft.irfftn(ret, axes=axes)
     ret = _cp.fft.fftshift(ret, axes=axes)[
-        tuple((Ellipsis, *(slice(ps // 2 - input.shape[ax], ps // 2 + input.shape[ax]) for ax, ps in zip(axes, fftshape))))
+        tuple(
+            (
+                Ellipsis,
+                *(slice(ps // 2 - input.shape[ax], ps // 2 + input.shape[ax]) for ax, ps in zip(axes, fftshape)),
+            )
+        )
     ]
     if norm:
         n = corr(_cp.ones(tuple(input.shape[ax] for ax in axes)), returngpu=True)
@@ -30,3 +35,87 @@ def corr(input, axes=(-1, -2), norm=False, returngpu=False, **kwargs):
         ret = _cp.asnumpy(ret)
         _cp.get_default_memory_pool().free_all_blocks()
     return ret
+
+
+_div = _cp.ElementwiseKernel("T x, T count", "T res", "if(count>0.5){res = x/count;} else {res = 0;}", "div")
+
+
+class correlator2d:
+    def __init__(self, mask=None, norm=True) -> None:
+        """Simple 2D correlator
+        Does the 2D autocorrelation divided by the mask autocorrelation.
+        mask: mask to use for the autocorrelation or None for all-ones mask
+        norm: normalize input by mean over all pixels.
+        """
+
+        self._fftshape = None
+        if mask is not None:
+            mask = _cp.asarray(mask, dtype=_cp.float32)
+            self._maskcorr = self._corr(mask)
+            self._mask = mask
+        else:
+            self._mask = None
+            self._maskcorr = None
+        self._mean = None
+        self._n = 0
+        self._nvar = None
+        self._norm = norm
+
+    def _corr(self, input: _cp.array) -> _cp.array:
+        if self._fftshape is None:
+            self._fftshape = [_fastlen(2 * s) for s in input.shape]
+        ret = _cp.fft.rfft2(input, s=self._fftshape)
+        ret = _cp.abs(ret) ** 2
+        ret = _cp.fft.irfft2(ret)
+        ret = _cp.fft.fftshift(ret, axes=(-2, -1))[
+            ...,
+            slice(self._fftshape[-2] // 2 - input.shape[-2], self._fftshape[-2] // 2 + input.shape[-2]),
+            slice(self._fftshape[-1] // 2 - input.shape[-1], self._fftshape[-1] // 2 + input.shape[-1]),
+        ]
+        return ret
+
+    def add(self, input: _np.array, mask=None) -> None:
+        if input.ndim != 2:
+            raise ValueError("input must be 2D.")
+        input = _cp.array(input, dtype=_cp.float32, copy=True)
+        if self._norm:
+            input /= _cp.mean(input)
+
+        if mask is not None:
+            input *= _cp.asarray(mask, dtype=_cp.float32)
+            maskcorr = self._corr(mask)
+        elif self._mask is not None:
+            input *= self._mask
+            maskcorr = self._maskcorr
+        elif self._maskcorr is not None:
+            maskcorr = self._maskcorr
+        else:
+            mask = _cp.ones_like(input)
+            maskcorr = self._corr(mask)
+            self._maskcorr = maskcorr
+
+        corr = self._corr(input)
+        corr = _div(corr, maskcorr)
+
+        if self._mean is None:
+            self._mean = corr
+            self._n = 1
+            self._nvar = _cp.zeros_like(corr)
+        else:
+            self._n += 1
+            delta = corr - self._mean
+            self._mean += delta / self._n
+            delta2 = corr - self._mean
+            self._nvar += delta * delta2
+
+    @property
+    def mean(self) -> _np.array:
+        return self._mean.get()
+
+    @property
+    def n(self) -> int:
+        return self._n
+
+    @property
+    def std(self) -> _np.array:
+        return _cp.sqrt(self._nvar / self._n).get()
