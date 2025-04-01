72e11a8397d82bb9afcb333db9db56f8838dd3c1
Felix Zimmermann <fzimmermann89@gmail.com>
2025-04-01 12:29:25 +0200

    change cuda detection

--- idi/simulation/__init__.py
+++ idi/simulation/__init__.py
@@ -1,35 +1,38 @@
 __all__ = ["cpu", "simobj", "cuda", "common", "time", "cutime", "simple"]
+import cupy as _cp
+import numba.cuda as _numba_cuda
 
 from . import cpu, simobj, time, simple
 from .common import *
 
+auto = None
+autotime = None
+
 try:
     from . import cuda
-
-    auto = cuda
+    from . import cutime
 except ImportError as _e:
     if any(x in str(_e.args[0]).lower() for x in ["cuda", "libcu", "cupy"]):
         import warnings as _w
 
-        _w.warn("cuda error. cuda time dependent simulation not imported. is cuda available and paths set?")
+        _w.warn("cuda error. cuda time simulation not imported. is cuda available and paths set?")
         auto = cpu
-    else:
-        print(_e)
-except Exception as _e:
-    print("Import exception on cuda:")
-    print(_e)
-try:
-    from . import cutime
-
-    autotime = cutime
-except (AttributeError, ImportError) as _e:
-    if any(x in str(_e.args[0]).lower() for x in ["cuda", "libcu", "cupy"]):
-        import warnings as _w
-
-        _w.warn("cuda error. cuda time dependent simulation not imported. is cuda available and paths set?")
         autotime = time
+        _cuda = False
     else:
         print(_e)
 except Exception as _e:
-    print("Import exception on cutime:")
+    print("Import exception:")
     print(_e)
+
+
+if not _cp.cuda.is_available() or not _numba_cuda.is_available():
+    _w.warn("cuda not available, using cpu version")
+    auto = cpu
+    autotime = time
+else:
+    auto = cuda
+    autotime = cutime
+
+del _cp
+del _numba_cuda
